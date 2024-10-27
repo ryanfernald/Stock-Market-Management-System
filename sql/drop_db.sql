cd3e5ef7acb8189b7e879ea85dbe76495bcf841f
ANikfarjam <ashkan.nikfarjam@sjsu.edu>
2024-10-27 02:02:29 -0700

    updated SQL schema please make sure you look at changes

--- sql/drop_db.sql
+++ sql/drop_db.sql
@@ -1,42 +1,12 @@
-# USE <db_name>;
-
--- Drop all tables
-DROP PROCEDURE IF EXISTS `drop_all_tables`;
-
-DELIMITER $$
-CREATE PROCEDURE `drop_all_tables`()
-BEGIN
-    DECLARE _done INT DEFAULT FALSE;
-    DECLARE _tableName VARCHAR(255);
-    DECLARE _cursor CURSOR FOR
-SELECT table_name
-FROM information_schema.TABLES
-WHERE table_schema = SCHEMA();
-DECLARE CONTINUE HANDLER FOR NOT FOUND SET _done = TRUE;
-
-    SET FOREIGN_KEY_CHECKS = 0;
-
-OPEN _cursor;
-
-REPEAT FETCH _cursor INTO _tableName;
-
-    IF NOT _done THEN
-        SET @stmt_sql = CONCAT('DROP TABLE ', _tableName);
-PREPARE stmt1 FROM @stmt_sql;
-EXECUTE stmt1;
-DEALLOCATE PREPARE stmt1;
-END IF;
-
-    UNTIL _done END REPEAT;
-
-CLOSE _cursor;
-SET FOREIGN_KEY_CHECKS = 1;
-END$$
-
-DELIMITER ;
-
-call drop_all_tables();
-
-DROP PROCEDURE IF EXISTS `drop_all_tables`;
-
-SHOW TABLES;
+-- USE <db_name>;
+use YHFinance;
+DROP TABLE IF EXISTS Watchlist;
+DROP TABLE IF EXISTS FundsWithdraw;
+DROP TABLE IF EXISTS FundsDeposit;
+DROP TABLE IF EXISTS UserBalance;
+DROP TABLE IF EXISTS Portfolio;
+DROP TABLE IF EXISTS MarketOrder;
+DROP TABLE IF EXISTS StockPrice;
+DROP TABLE IF EXISTS Stock;
+DROP TABLE IF EXISTS Sectors;
+DROP TABLE IF EXISTS User;
