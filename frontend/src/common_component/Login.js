54683446c422db7f26ff8c584a0404ac6b1388ee
Ryan <60462881+ryanfernald@users.noreply.github.com>
2024-10-31 01:10:44 -0700

    simple add nav bar to login page, and fix nav bar on landing page

--- frontend/src/common_component/Login.js
+++ frontend/src/common_component/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { auth } from '../firbaseconfig.js';
 import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import Navbar from './Navbar';
 
 import './styling/Login.css';
 function Login() {
@@ -36,30 +37,33 @@ function Login() {
   };
 
   return (
-    <div className="login-container">
-      <div className="login-box">
-        <h2>Login</h2>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleLogin}>Login</button>
-        <button onClick={handleGoogleLogin}>
-          <img src="https://img.icons8.com/color/16/000000/google-logo.png" alt="Google icon" />
-          Log in with Google
-        </button>
+    <div>
+      <Navbar />
+      <div className="login-container">
+        <div className="login-box">
+          <h2>Login</h2>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>Login</button>
+          <button onClick={handleGoogleLogin}>
+            <img src="https://img.icons8.com/color/16/000000/google-logo.png" alt="Google icon" />
+            Log in with Google
+          </button>
 
-        {/* New Sign Up Section */}
-        <p className="signup-text">Don't have an account?</p>
-        <button className="signup-button" onClick={handleSignUp}>Sign Up</button>
+          {/* New Sign Up Section */}
+          <p className="signup-text">Don't have an account?</p>
+          <button className="signup-button" onClick={handleSignUp}>Sign Up</button>
+        </div>
       </div>
     </div>
   );
