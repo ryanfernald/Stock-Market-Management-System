0e08a17114c854804a5466fe7011bdd290c6d525
Max <tools.shiny.01@icloud.com>
2024-10-31 21:15:45 -0700

    added more tests. all seems working

--- middle_layer/tests/database_tests.py
+++ middle_layer/tests/database_tests.py
@@ -17,6 +17,7 @@ def test_balance_add(user_id, amount):
     except Exception as e:
         print(e)
 
+# test_point
 # test_balance_add('user1', 1000)
 
 
@@ -29,7 +30,8 @@ def test_balance_withdraw(user_id, amount):
     except Exception as e:
         print(e)
 
-# test_balance_withdraw('user1', 10000)
+# test_point
+# test_balance_withdraw('user1', 1000)
 
 
 # works
@@ -45,8 +47,8 @@ def test_buying_stock(user_id, ticker, quantity):
     except Exception as e:
         print(e)
 
-# test_buying_stock('user1', "AAPL", 1)
-
+# test_point
+test_buying_stock('user1', "AAPL", 10)
 
 # works
 def test_selling_stock(user_id, ticker, quantity):
@@ -61,17 +63,16 @@ def test_selling_stock(user_id, ticker, quantity):
     except Exception as e:
         print(e)
 
-
-# test_selling_stock('user1', "AAPL", 1)
+# test_point
+# test_selling_stock('user1', "BAC", 10)
 
 
 # works
 def test_transaction_history(user_id):
     print("Transaction History for", user_id)
-    transactions = db.get_user_transaction_history(user_id)
-    for transaction in transactions:
-        print(transaction)
+    print(db.get_user_transaction_history(user_id))
 
+# test_point
 # test_transaction_history('user1')
 
 
@@ -79,6 +80,7 @@ def test_transaction_history(user_id):
 def test_get_portfolio(user_id):
     print(f"portfolio:\n{db.get_user_portfolio(user_id)}")
 
+# test_point
 # test_get_portfolio('user1')
 
 
@@ -86,6 +88,7 @@ def test_get_portfolio(user_id):
 def test_get_portfolio_value(user_id):
     print(f"portfolio value: ${db.get_user_portfolio_value(user_id)}")
 
+# test_point
 # test_get_portfolio_value('user1')
 
 
@@ -93,4 +96,24 @@ def test_get_portfolio_value(user_id):
 def test_get_balance(user_id):
     print(f"balance : ${db.get_user_balance(user_id)}")
 
-test_get_balance('user1')
+# test_point
+# test_get_balance('user1')
+
+# works
+def test_insert_stock_price(ticker, price):
+    print(f"before ${db.get_most_recent_stock_price(ticker)}")
+    db.add_stock_price(ticker, price)
+    print(f"after ${db.get_most_recent_stock_price(ticker)}")
+
+# test_point
+# test_insert_stock_price("BAC", 80)
+
+
+# testing
+def test_insert_user(user_id, first_name, last_name, email):
+    print(f"before {db.get_user_data(user_id)}")
+    db.add_user(user_id, first_name, last_name, email)
+    print(f"after {db.get_user_data(user_id)}")
+
+# test_insert_user('user4', 'maxim', 'dokukin', 'mda@sjsu')
+
