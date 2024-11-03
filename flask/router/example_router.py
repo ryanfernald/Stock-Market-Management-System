f84a267dfda6910127cb9120c7ba41bb3165c803
Sean Hsieh <seankh4444@gmail.com>
2024-11-03 13:12:35 -0800

    Nov 3 setup Flask client

---
+++ flask/router/example_router.py
@@ -0,0 +1,11 @@
+from flask import Blueprint
+# Create a Blueprint named "example"
+example = Blueprint('example', __name__)
+
+@example.route('/example', methods=['GET'])
+def example_route():
+    return {"message": "Hello from example route!"}, 200 # set the response code to OK   
+
+@example.route('/example/<int:id>', methods=['GET'])
+def example_route2(id):
+    return {"message": f"The id is: {id}"}, 201 # set the response code to CREATED
