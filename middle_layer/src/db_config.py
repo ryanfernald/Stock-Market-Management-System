0be0ad091ca0f56998d74798f748dfc57f02d151
ANikfarjam <ashkan.nikfarjam@sjsu.edu>
2024-11-09 01:06:21 -0800

    minot adjustment it is working now

--- middle_layer/src/db_config.py
+++ middle_layer/src/db_config.py
@@ -1,9 +1,11 @@
+from dotenv import load_dotenv
 import os
 from classes.DataBase import DataBase
+load_dotenv()
 db = DataBase(
     host=os.getenv("DATABASE_HOST"),
     user=os.getenv("DATABASE_USER"),
-    password=os.getenv("DATABASE_USER"),
+    password=os.getenv("DATABASE_PASSWORD"),
     database=os.getenv("DATABASE_NAME"),
     port=os.getenv("DATABASE_PORT", 3306),
     db='mysql'
