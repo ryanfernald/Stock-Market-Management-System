62f6ade986ffae1c1e72db86ff6dd0ed10a7bfff
Ashkan Nikfarjam <ashkan_nikfarjam@yahoo.com>
2024-11-15 14:52:50 -0800

    AdminDashboard now can add data to tables, ***Note***: not all the tables are sinced with recent sql schema, all add that later todat

--- frontend/src/admin_coponent/AdminTableManip.js
+++ frontend/src/admin_coponent/AdminTableManip.js
@@ -46,7 +46,7 @@ const AdminInsert = () => {
             };
     
             // Perform the fetch call
-            const response = await fetch('http://localhost:5000/Admin/insertion', requestOptions);
+            const response = await fetch('http://127.0.0.1:5000/Admin/insertion', requestOptions);
     
             // Check if the response is successful
             if (response.ok) {
