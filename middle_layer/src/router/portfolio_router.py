a06e827b17df73d68229227587028a4f7cf82788
Ryan <60462881+ryanfernald@users.noreply.github.com>
2024-12-05 13:25:13 -0800

    Memory leak DB issue, fixed user logged in navbar buttons

--- middle_layer/src/router/portfolio_router.py
+++ middle_layer/src/router/portfolio_router.py
@@ -10,5 +10,5 @@ def get_user_portfolio(user_id):
 
 @portfolio.route('/value/<user_id>', methods=['GET'])
 def get_user_portfolio_value(user_id):
-
+    print("portfolio get value called for user " + user_id)
     return db.get_user_portfolio_value(user_id)
